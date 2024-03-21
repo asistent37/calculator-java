c5813b01a82506628162f459997cbe8fe9ad49f1
asistent37 <124823459+asistent37@users.noreply.github.com>
2024-03-21 01:55:09 +0100

    Add files via upload

---
+++ SimpleTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class SimpleTest {
+    @Test
+    public void testCalculate() {
+        //assert statements
+        assertEquals("Calculator calculate complete arithmetical expression 4+5",(9));
+        assertEquals("Calculator supports the priority of calculation operations 10+5*4+3", (33));
+    
+        
+        
+        }
+    
+}
